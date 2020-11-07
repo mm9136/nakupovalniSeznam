93a9608f39e678ae8fdc2dd421d4efc644c105eb
mm9136 <mm9136@student.uni-lj.si>
2020-11-07 20:28:58 +0100

    modified

--- api/src/main/java/si/fri/prpo/skupina14/servlet/JPAServlet.java
+++ api/src/main/java/si/fri/prpo/skupina14/servlet/JPAServlet.java
@@ -1,6 +1,6 @@
 package si.fri.prpo.skupina14.servlet;
-import si.fri.prpo.skupina14.nakupovalniSeznam.entitete.Uporabnik;
-import si.fri.prpo.skupina14.zrna.UporabnikZrno;
+import si.fri.prpo.skupina14.nakupovalniSeznam.entitete.*;
+import si.fri.prpo.skupina14.zrna.*;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -20,27 +20,20 @@ public class JPAServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        List<Uporabnik> uporabniki = uporabnikZrno.pridobiVseUporabnike();
-        List<Uporabnik> uporabnikiCriteria = uporabnikZrno.getUporabnikiCriteriaAPI();
-        List<String> uporabnikZDanimEmailom = uporabnikZrno.getImeInPriimekUporabnika("mmilsss");
-
         resp.setContentType("text/html; charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
 
         PrintWriter writer = resp.getWriter();
 
         //izpis vseh uporabnikov
+        List<Uporabnik> uporabniki = uporabnikZrno.pridobiVseUporabnike();
         writer.println("<h2>1) Izpis uporabnikov iz podatkovne baze:</h2>");
         uporabniki.forEach( u -> writer.println("<p>"+ u.toString() + "</p>"));
 
         //izpis vseh uporabnikov s CriteriaAPI
         writer.println("<h2>2) Izpis uporabnikov iz podatkovne baze z CriteriaAPI:</h2>");
+        List<Uporabnik> uporabnikiCriteria = uporabnikZrno.getUporabnikiCriteriaAPI();
         uporabnikiCriteria.forEach( u -> writer.println("<p>"+ u.toString() + "</p>"));
-
-        //izpis uporabnikovega imena in priimka z danim emailom
-        writer.println("<h2>3) Izpis  uporabnika z danim emailom:</h2>");
-        uporabnikZDanimEmailom.forEach( u -> writer.println("<p>"+ u.toString() + "</p>"));
 
     }
 }
--- storitve/src/main/java/si/fri/prpo/skupina14/zrna/UporabnikZrno.java
+++ storitve/src/main/java/si/fri/prpo/skupina14/zrna/UporabnikZrno.java
@@ -1,6 +1,9 @@
 package si.fri.prpo.skupina14.zrna;
 
-import si.fri.prpo.skupina14.nakupovalniSeznam.entitete.Uporabnik;
+import si.fri.prpo.skupina14.nakupovalniSeznam.entitete.*;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -8,48 +11,23 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.UUID;
 
 @ApplicationScoped
 public class UporabnikZrno {
 
     @PersistenceContext(unitName = "uporabniki-jpa")
     private EntityManager em;
+    private Logger log = Logger.getLogger(UporabnikZrno.class.getName());
+    private String idZrna;
 
     //getAll users
     public List<Uporabnik> pridobiVseUporabnike() {
         List<Uporabnik> uporabniki = em.createNamedQuery("Uporabnik.getAll").getResultList();
         return uporabniki;
-    }
-    //getFullNameOfAUser with his username
-    public List<String> getImeInPriimekUporabnika(String uporabniskoIme) {
-        List<String> imenaInPriimki = em.createNamedQuery("Uporabnik.getFullNameOfAUser").setParameter("uporabniskoIme", uporabniskoIme).getResultList();
-        return imenaInPriimki;
-    }
-    //getAllUserNames
-    public List<String> getVsaUporabniskaImena() {
-        List<String> uporabniskaImena = em.createNamedQuery("Uporabnik.getAllUserNames").getResultList();
-        return uporabniskaImena;
-    }
-    //getUserFromUserName
-    public Uporabnik getUporabnikZUporabniskimImenom (String uporabniskoIme) {
-        Uporabnik uporabnik = null;
-        try {
-            uporabnik = (Uporabnik) em.createNamedQuery("Uporabnik.getUserFromUserName").setParameter("uporabniskoIme", uporabniskoIme).getSingleResult();
-        }
-        catch (NoResultException nre){
-            //Ignore this because as per your logic this is ok!
-        }
-        return uporabnik;
-    }
-    //getUserFromEmail
-    public Uporabnik getUporabnikZEmailom (String email) {
-        Uporabnik uporabnik = null;
-        try {
-            uporabnik = (Uporabnik)em.createNamedQuery("Uporabnik.getUserFromEmail").setParameter("email", email).getSingleResult();
-        }
-        catch (NoResultException nre) { }
-        return uporabnik;
     }
 
     //vrnitev vseh uporabnikov z CriteriaAPI
@@ -60,5 +38,6 @@ public class UporabnikZrno {
         query.select(from);
         return em.createQuery(query).getResultList();
     }
+
 
 }
