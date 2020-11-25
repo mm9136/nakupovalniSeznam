37c6d2264d4c05889753f522b8e36969aa870f50
mm9136 <mm9136@student.uni-lj.si>
2020-11-25 02:14:39 +0100

    vaje 5 razlicica 3

--- api/src/main/java/si/fri/prpo/skupina14/api/v1/NakupovalniSeznamiApplication.java
+++ api/src/main/java/si/fri/prpo/skupina14/api/v1/NakupovalniSeznamiApplication.java
@@ -1,8 +1,9 @@
-package si.fri.prpo.skupina14.api;
+package si.fri.prpo.skupina14.api.v1;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.*;
 @ApplicationPath("v1")
+//to je naloga sa vaja 5
 public class NakupovalniSeznamiApplication extends Application {
 
 }
